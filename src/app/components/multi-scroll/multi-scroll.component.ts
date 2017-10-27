36953e244d62f2204b69cdf30f02d7f31426a512
Derrick Fox <derrick.fox2@nih.gov>
2017-10-27 17:33:10 -0400

    Improve multislider

--- src/app/components/multi-scroll/multi-scroll.component.ts
+++ src/app/components/multi-scroll/multi-scroll.component.ts
@@ -9,10 +9,28 @@ export class MultiScrollComponent implements OnInit, AfterViewInit{
 
   @ViewChild("carouselExampleIndicators") carouselExampleIndicators: ElementRef;
 
+  slidesArray: any;
+
   constructor() { }
 
   ngOnInit() {
-
+    this.slidesArray = [
+      {
+        label: 'Slide One',
+        image: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/pug-dog-breed-pictures/3-sidesitting.jpg',
+        link: 'http://www.google.com'
+      },
+      {
+        label: 'Slide Two',
+        image: 'http://media.gettyimages.com/photos/surprised-cat-picture-id561460035',
+        link: 'http://www.google.com'
+      },
+      {
+        label: 'Slide Three',
+        image: 'http://dogsaholic.com/wp-content/uploads/2015/04/Dumb-face.jpg',
+        link: 'http://www.google.com'
+      }
+    ]
   }
 
   ngAfterViewInit() {
